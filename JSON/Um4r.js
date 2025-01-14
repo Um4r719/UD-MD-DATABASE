77068cad6723a5f983ceddde2c739f6ed4617f8e
Ɗᴀʀᴋ Ɗєνιʟ 👑🇵🇰🖤 <umarofficial404@gmail.com>
2025-01-14 14:27:26 +0500

    Create Um4r.js

---
+++ JSON/Um4r.js
@@ -0,0 +1,6 @@
+{
+  "id": "um4rxd",
+  "image": "https://imgur.com/a/vcCqWvx",
+  "title": "UMAR 3.O WHATSAPP BOT",
+  "text": "\n_This is the result of umar's hard work and umar owns the bot's rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances._\n\n🔰 *Official GitHub Repo* - ```https://github.com/Um4r719/UD-MD```\n\n🪀 *WhatsApp Channel* - ```https://whatsapp.com/channel/0029VawphngId7nHJrotee3Q```"
+}
